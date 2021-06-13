199785772f3557ea3faecc80125e99ee2badaecb
Aldin Philo <aldinphilo@gmail.com>
2021-06-13 23:53:39 +0530

    template created

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
 if settings.DEBUG:
     urlpatterns += static(
