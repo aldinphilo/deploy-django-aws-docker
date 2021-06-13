ee850ea6de10896e77d489781bfe5713297f60d0
Aldin Philo <aldinphilo@gmail.com>
2021-06-14 00:01:17 +0530

    fix url bug

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home
+from core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
